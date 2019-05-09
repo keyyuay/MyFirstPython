fb4ca44c407d80b9ef6d1c17bb34628a8047a6a6
keyyuay <mkeycnn@gmail.com>
2019-05-10 02:31:46 +0700

    testing

---
+++ main.py
@@ -0,0 +1,29 @@
+#number = input("enter number :")
+#print ("your lucky number is " + number )
+
+key1 = "real"
+key2 = 555568
+
+print (type(key1))
+print (type(key2))
+
+var = "00011"
+print(int(var))
+
+float1 = 1.22
+float2 = 2.44
+
+print(int(float1 + float2))
+
+num = 10**4 / 100+100
+print (num)
+
+print (2 == 3)
+
+print(int(False))
+print(int(True))
+
+text1 = "cherry"
+text2 = "bomp"
+text3 = text1, text2
+print(text3)
